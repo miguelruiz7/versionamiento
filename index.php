7276ea53461f03541eadd5d3369d152d6bfa7a34
Miguel Ruiz Zamora <58411768+miguelruiz7@users.noreply.github.com>
2024-06-18 20:29:44 -0600

    Creación de proyecto

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis intereses</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container px-4 py-5" id="featured-3">
+    <h2 class="pb-2 border-bottom">Mis intereses</h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Música</h3>
+        <p>Es uno de mis principales intereses, porque de cierto modo despierta la creatividad que llevo dentro.</p>
+        <!-- <a href="#" class="icon-link">
+          Call to action
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a> -->
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Amor</h3>
+        <p>A quien no le hace falta un poquito de amor de afuera. Por su puesto que yo, desde mi prespectiva crezco un poquito como persona y me da cierta seguridad en eso.</p>
+      <!--  <a href="#" class="icon-link">
+          Call to action
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a> -->
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Programación</h3>
+        <p>Al principio que lo conocí no me gustaba, ni sabia porque estaba aquí, pero ahora se ha vuelto parte indispensable de mi ser</p>
+      <!--  <a href="#" class="icon-link">
+          Call to action
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a> -->
+      </div>
+    </div>
+  </div>
+    
+</body>
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</html>
